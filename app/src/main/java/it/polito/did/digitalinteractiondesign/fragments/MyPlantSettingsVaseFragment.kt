e35076b156d2b28b08be28b5f0428d6b5a9c8143
mara <mara.salazzari@gmail.com>
2022-10-19 20:12:15 +0200

    modifiche chiamata

--- app/src/main/java/it/polito/did/digitalinteractiondesign/fragments/MyPlantSettingsVaseFragment.kt
+++ app/src/main/java/it/polito/did/digitalinteractiondesign/fragments/MyPlantSettingsVaseFragment.kt
@@ -2,13 +2,13 @@ package it.polito.did.digitalinteractiondesign.fragments
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.ImageButton
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -50,6 +50,7 @@ class MyPlantSettingsVaseFragment : Fragment() {
         val soilType = resources.getStringArray(R.array.soil_type)
         val soilTypeArrayAdapter = ArrayAdapter(requireContext(), R.layout.item_dropdown_vase_settings, soilType)
         binding.textViewSoilType.setAdapter(soilTypeArrayAdapter)
+        binding.textViewSoilType.setText("Type 3", false);
 
         return binding.root
     }
